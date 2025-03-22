6ce0f23ac5692cd2b76dc1159db4d0f3823bfb76
cyqhanser985 <2660920607@qq.com>
2025-03-22 15:39:51 +0800

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+root = tk.TK()
+
+left_frame = tk.Frame(root)
+left_frame.pack(side=tk.LEFT,fill=tk.Y)
+tk.Button(left_frame,text="RED",bg="red").pack(fill=tk.X)
+tk.Button(left_frame,text=)
