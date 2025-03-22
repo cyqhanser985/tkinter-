b1c5a45e4abc69578c852a49ded3065705bd0036
cyqhanser985 <2660920607@qq.com>
2025-03-22 16:14:58 +0800

    Delete test1.py

--- test1.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-root = tk.TK()
-
-left_frame = tk.Frame(root)
-left_frame.pack(side=tk.LEFT,fill=tk.Y)
-tk.Button(left_frame,text="RED",bg="red").pack(fill=tk.X)
-tk.Button(left_frame,text=)
